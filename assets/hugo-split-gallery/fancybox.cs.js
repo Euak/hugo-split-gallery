a7063e414cae459fac238e32136bbfc78427f408
Bedřich Valenta <bevapost@mailbox.org>
2022-02-27 16:08:46 +0000

    Theme Language support for de-de cs-cz

---
+++ assets/hugo-split-gallery/fancybox.cs.js
@@ -0,0 +1,14 @@
+$.fancybox.defaults.i18n['cs'] = {
+    CLOSE: "Zavřít",
+    NEXT: "Následující",
+    PREV: "Předchozí",
+    ERROR: "Obsah není možné načíst. <br/> Prosím, zkuste to později.",
+    PLAY_START: "Spustit prezentaci",
+    PLAY_STOP: "Pozastavit prezentaci",
+    FULL_SCREEN: "Na celé obrazovce",
+    THUMBS: "Thumbnails",
+    DOWNLOAD: "Stáhnout",
+    SHARE: "Sdílet",
+    ZOOM: "Zoom"
+};
+$.fancybox.defaults.lang = "cs"
